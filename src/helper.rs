5c4b01bd640f49e7ab5bcc60705f4fa6a89df683
ccdle12 <chris.coverdale24@gmail.com>
2019-04-26 13:05:20 +0900

    src/helper: adding sign_switch to help with negative num ordering

--- src/helper.rs
+++ src/helper.rs
@@ -14,31 +14,64 @@ pub const RADIX: u32 = 10;
 /// enum of Ordering. This is primarily used in the Ord trait implementation.
 pub fn compare_num(x: (&BigDigit, bool), y: (&BigDigit, bool)) -> Ordering {
     // Check and compare the sign first.
+    // x is negative and why is positive.
     if x.1 && !y.1 {
         return Less;
     }
 
-    let (x_len, y_len) = (x.0.len(), y.0.len());
-
-    if x_len < y_len {
-        return Less;
-    }
-
-    if x_len > y_len {
+    // x is positive and y is negative.
+    if !x.1 && y.1 {
         return Greater;
     }
 
+    // Switch on the Ordering according to the sign.
+    let switch: bool = x.1 && y.1;
+
+    // Compare the lengths.
+    let (x_len, y_len) = (x.0.len(), y.0.len());
+
+    if x_len < y_len {
+        return sign_switch(switch, Less);
+    }
+
+    if x_len > y_len {
+        return sign_switch(switch, Greater);
+    }
+
+    // Compare each primitive digit.
     for (&xi, &yi) in x.0.iter().rev().zip(y.0.iter().rev()) {
         if xi < yi {
-            return Less;
+            return sign_switch(switch, Less);
         }
 
         if xi > yi {
-            return Greater;
+            return sign_switch(switch, Greater);
         }
     }
 
     Equal
+}
+
+// internal helper function, given the sign and the positive expected Ordering,
+// the function will switch return the positive expected Ordering if b is true,
+// and return the switch (opposite) Ordering if b is false.
+fn sign_switch(b: bool, positive_ord: Ordering) -> Ordering {
+    match b {
+        true => {
+            return match positive_ord {
+                Less => Greater,
+                Greater => Less,
+                _ => Equal,
+            };
+        }
+        false => {
+            return match positive_ord {
+                Less => Less,
+                Greater => Greater,
+                _ => Equal,
+            };
+        }
+    }
 }
 
 /// A helper function to remove any leading zeroes from a BigDigit.
